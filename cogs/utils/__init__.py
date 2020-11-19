0a04dd2733690033fe2b5da4c8f150fb7da06f70
Callum Bartlett <callum@voxelfox.co.uk>
2020-11-19 14:54:29 +0000

    Update utils init file

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,16 +1,5 @@
 # flake8: noqa
-from cogs.utils import checks, converters, errors
-from cogs.utils.checks import cooldown
-from cogs.utils.context_embed import ContextEmbed as Embed
-from cogs.utils.custom_bot import CustomBot as Bot
-from cogs.utils.custom_cog import CustomCog as Cog
-from cogs.utils.custom_command import CustomCommand as Command
-from cogs.utils.custom_command import CustomGroup as Group
-from cogs.utils.custom_context import CustomContext as Context
-from cogs.utils.database import DatabaseConnection
-from cogs.utils.redis import RedisConnection
-from cogs.utils.time_value import TimeValue
-from cogs.utils.settings_menu import SettingsMenu, SettingsMenuOption, SettingsMenuIterable, SettingsMenuIterableBase
+from cogs.utils import checks, errors
 from cogs.utils.profiles.field import Field
 from cogs.utils.profiles.field_type import FieldType, TextField, NumberField, ImageField
 from cogs.utils.profiles.template import Template

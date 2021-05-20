10046b47f3aa5c3fb7d55ec3632c9b6b4e099207
Kae Bartlett <Kae@voxelfox.co.uk>
2021-05-20 04:43:57 +0100

    Add guilds page

--- website/frontend.py
+++ website/frontend.py
@@ -9,7 +9,7 @@ routes = RouteTableDef()
 
 
 @routes.get("/")
-@template("index.html.j2")
+@template("index.htm.j2")
 @webutils.add_discord_arguments()
 async def index(request: Request):
     """
@@ -17,3 +17,22 @@ async def index(request: Request):
     """
 
     return {}
+
+
+@routes.get("/guilds")
+@webutils.requires_login()
+@template("guilds.htm.j2")
+@webutils.add_discord_arguments()
+async def guilds(request: Request):
+    """
+    The guild picker page for the bot.
+    """
+
+    all_guilds = await webutils.get_user_guilds_from_session(request)
+    valid_guilds = [
+        i for i in all_guilds if i['owner'] or discord.Permissions(i['permissions']).manage_guild
+    ]
+    return {
+        "guilds": valid_guilds,
+    }
+

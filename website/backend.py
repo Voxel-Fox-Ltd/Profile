5b717e3498b8f08393dd2ab68473ca9631a5635a
Kae Bartlett <kae@voxelfox.co.uk>
2021-05-27 13:16:08 +0100

    Allow users to edit templates from the website

--- website/backend.py
+++ website/backend.py
@@ -1,8 +1,9 @@
-from aiohttp.web import HTTPFound, Request, Response, RouteTableDef
+from aiohttp.web import HTTPFound, Request, Response, RouteTableDef, json_response
 from voxelbotutils import web as webutils
 import aiohttp_session
 import discord
-from aiohttp_jinja2 import template
+
+from cogs import utils as localutils
 
 
 routes = RouteTableDef()
@@ -39,3 +40,123 @@ async def login(request: Request):
     """
 
     return HTTPFound(location=webutils.get_discord_login_url(request, "/login_processor"))
+
+
+@routes.post("/api/update_template")
+async def update_template(request: Request):
+    """
+    The route that handles the updating of the template metadata.
+    """
+
+    # Make sure the user is logged in
+    session = await aiohttp_session.get_session(request)
+    if not session.get("user_id"):
+        return json_response({"error": "Not logged in."}, status=401)
+
+    # Try and read the POST data from the user
+    required_fields = {"template_id", "verification_channel_id", "archive_channel_id", "role_id", "max_profile_count"}
+    field_converters = {"max_profile_count": int}
+    try:
+        data = await request.json()
+        data = {i: o.strip() or None for i, o in data.items()}
+        for f in required_fields:
+            data.setdefault(f, None)
+            if f in field_converters:
+                data[f] = field_converters[f](data[f])
+    except ValueError:
+        return json_response({"error": "Couldn't validate the given POST data."}, status=400)
+    except Exception:
+        return json_response({"error": "Could not read POST data."}, status=400)
+
+    # Now we want to check things with the database
+    async with request.app['database']() as db:
+
+        # Make sure the template exists
+        template = await localutils.Template.fetch_template_by_id(db, data['template_id'], fetch_fields=False)
+        if not template:
+            return json_response({"error": "Template does not exist."}, status=400)
+
+        # Make sure the user is editing a template that they have permission to edit
+        try:
+            guild = await request.app['bots']['bot'].fetch_guild(template.guild_id)
+        except discord.HTTPException:
+            return json_response({"error": "Bot not in guild."}, status=401)
+        try:
+            member = await guild.fetch_member(session.get("user_id"))
+        except discord.HTTPException:
+            return json_response({"error": "User not in guild."}, status=401)
+        if not member.guild_permissions.manage_guild:
+            return json_response({"error": "Member cannot manage guild."}, status=401)
+
+        # Update the template
+        updated_rows = await db(
+            """UPDATE template SET verification_channel_id=$2, archive_channel_id=$3, role_id=$4,
+            max_profile_count=$5 WHERE template_id=$1""",
+            data['template_id'], data['verification_channel_id'], data['archive_channel_id'],
+            data['role_id'], data['max_profile_count'],
+        )
+
+    # Return
+    return json_response({"error": ""}, status=200)
+
+
+@routes.post("/api/update_template_field")
+async def update_template_field(request: Request):
+    """
+    The route that handles the updating of the template metadata.
+    """
+
+    # Make sure the user is logged in
+    session = await aiohttp_session.get_session(request)
+    if not session.get("user_id"):
+        return json_response({"error": "Not logged in."}, status=401)
+
+    # Try and read the POST data from the user
+    required_fields = {"field_id", "name", "prompt", "timeout", "type", "optional"}
+    field_converters = {"timeout": int, "optional": bool}
+    try:
+        data = await request.json()
+        data = {i: o.strip() or None for i, o in data.items()}
+        for f in required_fields:
+            data.setdefault(f, None)
+            if f in field_converters:
+                data[f] = field_converters[f](data[f])
+    except ValueError:
+        return json_response({"error": "Couldn't validate the given POST data."}, status=400)
+    except Exception:
+        return json_response({"error": "Could not read POST data."}, status=400)
+
+    # Now we want to check things with the database
+    async with request.app['database']() as db:
+
+        # Make sure the template exists
+        field_rows = await db("""SELECT * FROM field WHERE field_id=$1""", data['field_id'])
+        if not field_rows:
+            return json_response({"error": "Field does not exist."}, status=400)
+        field = localutils.Field(**field_rows[0])
+        template = await localutils.Template.fetch_template_by_id(db, field.template_id, fetch_fields=False)
+        if not template:
+            return json_response({"error": "Template does not exist."}, status=400)
+
+        # Make sure the user is editing a template that they have permission to edit
+        try:
+            guild = await request.app['bots']['bot'].fetch_guild(template.guild_id)
+        except discord.HTTPException:
+            return json_response({"error": "Bot not in guild."}, status=401)
+        try:
+            member = await guild.fetch_member(session.get("user_id"))
+        except discord.HTTPException:
+            return json_response({"error": "User not in guild."}, status=401)
+        if not member.guild_permissions.manage_guild:
+            return json_response({"error": "Member cannot manage guild."}, status=401)
+
+        # Update the template
+        updated_rows = await db(
+            """UPDATE field SET name=$2, prompt=$3, timeout=$4, field_type=$5,
+            optional=$6 WHERE field_id=$1""",
+            data['field_id'], data['name'], data['prompt'], data['timeout'],
+            data['type'], data['optional'],
+        )
+
+    # Return
+    return json_response({"error": ""}, status=200)
--- website/static/js/template_edit.js
+++ website/static/js/template_edit.js
@@ -1,7 +1,12 @@
-async function getFieldSubmitButton(node) {
+async function getField(node) {
     while(!node.className.includes("field")) {
         node = node.parentNode;
     }
+    return node;
+}
+
+async function getFieldSubmitButton(node) {
+    node = await getField(node);
     return node.getElementsByClassName("submit-button")[0];
 }
 
@@ -15,6 +20,55 @@ async function enableFieldSubmitButton(node) {
 async function disableFieldSubmitButton(node) {
     b = await getFieldSubmitButton(node);
     b.disabled = true;
+}
+
+
+async function sendUpdateTemplate(node) {
+    field = await getField(node);
+    data = {
+        template_id: field.querySelector('[name=template_id]').value,
+        verification_channel_id: field.querySelector('[name=verification_channel_id]').value,
+        archive_channel_id: field.querySelector('[name=archive_channel_id]').value,
+        role_id: field.querySelector('[name=role_id]').value,
+        max_profile_count: field.querySelector('[name=max_profile_count]').value,
+    };
+    site = await fetch("/api/update_template", {
+        method: "POST",
+        body: JSON.stringify(data)
+    });
+    if(site.ok) {
+        alert("Updated template.");
+        node.disabled = true;
+    }
+    else {
+        body = await site.text()
+        alert(`Failed to update template - ${body}.`);
+    }
+}
+
+
+async function sendUpdateField(node) {
+    field = await getField(node);
+    data = {
+        field_id: field.querySelector('[name=field_id]').value,
+        name: field.querySelector('[name=name]').value,
+        prompt: field.querySelector('[name=prompt]').value,
+        timeout: field.querySelector('[name=timeout]').value,
+        type: field.querySelector('[name=type]').value,
+        optional: field.querySelector('[name=optional]').value,
+    };
+    site = await fetch("/api/update_template_field", {
+        method: "POST",
+        body: JSON.stringify(data)
+    });
+    if(site.ok) {
+        alert("Updated field.");
+        node.disabled = true;
+    }
+    else {
+        body = await site.text()
+        alert(`Failed to update field - ${body}.`);
+    }
 }
 
 

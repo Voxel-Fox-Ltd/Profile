2837e75896b242295eb3ae6e7e6666fd02b1fc55
Kae Bartlett <kae@voxelfox.co.uk>
2022-03-24 01:53:37 +0000

    Make profile name and user required for profile get

--- cogs/profile_commands.py
+++ cogs/profile_commands.py
@@ -940,7 +940,7 @@ class ProfileCommands(vbu.Cog):
             ctx: utils.types.GuildContext,
             user: typing.Optional[discord.Member] = None,
             *,
-            profile_name: str = None,
+            profile_name: str,
             ):
         """
         Gets a profile for a given member.

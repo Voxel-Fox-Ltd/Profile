f220d3f6e78ce6d97274851de1086ab951810886
Kae Bartlett <kae@voxelfox.co.uk>
2022-06-03 18:06:18 +0100

    Ignore attributeerrors

--- cogs/profile_commands.py
+++ cogs/profile_commands.py
@@ -145,6 +145,8 @@ class ProfileCommands(vbu.Cog):
         if not template:
             try:
                 await ctx.guild.delete_application_command(discord.Object(ctx.interaction.data['id']))
+            except AttributeError:
+                pass
             except Exception as e:
                 self.logger.error("failed to delete command", exc_info=e)
             return  # Fail silently on template doesn't exist

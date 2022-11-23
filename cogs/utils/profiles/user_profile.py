cd7da89100f4bc07497f2156daed1317bd80f5c8
Kae Bartlett <kae@voxelfox.co.uk>
2022-11-23 17:09:42 +0000

    Code formatting

--- cogs/utils/profiles/user_profile.py
+++ cogs/utils/profiles/user_profile.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import Union, Optional, Dict, List
+from typing_extensions import Self
 import uuid
 import operator
 
@@ -10,15 +11,21 @@ from discord.ext import commands, vbu
 from cogs.utils.profiles.template import Template
 from cogs.utils.profiles.filled_field import FilledField
 from cogs.utils.profiles.field_type import ImageField
-from cogs.utils.profiles.command_processor import CommandProcessor, InvalidCommandText
+from cogs.utils.profiles.command_processor import (
+    CommandProcessor,
+    InvalidCommandText
+)
 
 
 class UserProfile(object):
     """
     A filled user template.
-    This represents a template filled by a user containing all of their relevant information.
-    This class itself does not contain the user's data, but rather the metadata about their given profile,
-    eg whether it's been verified etc; the user's data is stored inthe FilledField objects associated
+    This represents a template filled by a user containing all of their
+    relevant information.
+    This class itself does not contain the user's data, but rather the
+    metadata about their given profile,
+    eg whether it's been verified etc; the user's data is stored inthe
+    FilledField objects associated
     with this.
 
     Parameters
@@ -32,7 +39,8 @@ class UserProfile(object):
     verified: :class:`bool`
         Whether or not the profile has been verified.
     posted_message_id: Optional[:class:`int`]
-        The ID of the message that was posted into the archive or verification channel.
+        The ID of the message that was posted into the archive or
+        verification channel.
     posted_channel_id: Optional[:class:`int`]
         The ID of the message's channel that was posted into the
         archive or verification channel.
@@ -50,7 +58,8 @@ class UserProfile(object):
     verified: :class:`bool`
         Whether or not the profile has been verified.
     posted_message_id: Optional[:class:`int`]
-        The ID of the message that was posted into the archive or verification channel.
+        The ID of the message that was posted into the archive or
+        verification channel.
     posted_channel_id: Optional[:class:`int`]
         The ID of the message's channel that was posted into the
         archive or verification channel.
@@ -61,8 +70,15 @@ class UserProfile(object):
     """
 
     __slots__ = (
-        "user_id", "name", "template_id", "verified", "all_filled_fields",
-        "template", "posted_message_id", "posted_channel_id",
+        "user_id",
+        "name",
+        "template_id",
+        "verified",
+        "all_filled_fields",
+        "template",
+        "posted_message_id",
+        "posted_channel_id",
+        "deleted",
     )
 
     def __init__(
@@ -73,13 +89,15 @@ class UserProfile(object):
             verified: bool,
             posted_message_id: Optional[int] = None,
             posted_channel_id: Optional[int] = None,
-            template: Optional[Template] = None):
+            template: Optional[Template] = None,
+            deleted: bool = False):
         self.user_id: int = user_id
         self.name: str = name
         self.template_id: str = str(template_id)
         self.verified: bool = verified
         self.posted_message_id = posted_message_id
         self.posted_channel_id = posted_channel_id
+        self.deleted: bool = deleted
         self.all_filled_fields: Dict[str, FilledField] = dict()
         self.template: Optional[Template] = template
 
@@ -98,7 +116,18 @@ class UserProfile(object):
 
         # Get the fields that have been filled in
         field_rows = await db(
-            "SELECT * FROM filled_field WHERE user_id=$1 AND name=$2 AND field_id=ANY($3::UUID[])",
+            """
+            SELECT
+                *
+            FROM
+                filled_field
+            WHERE
+                user_id = $1
+            AND
+                name = $2
+            AND
+                field_id = ANY($3::UUID[])
+            """,
             self.user_id, self.name, self.template.all_fields.keys(),
         )
         self.all_filled_fields.clear()
@@ -112,16 +141,26 @@ class UserProfile(object):
         # And return
         return self.all_filled_fields
 
-    async def fetch_template(self, db, *, fetch_fields: bool = True) -> Optional[Template]:
+    async def fetch_template(
+            self,
+            db: vbu.Database,
+            *,
+            fetch_fields: bool = True) -> Optional[Template]:
         """
         Fetch the template for this field and store it in .template.
         """
 
-        template = await Template.fetch_template_by_id(db, self.template_id, fetch_fields=fetch_fields)
+        template = await Template.fetch_template_by_id(
+            db,
+            self.template_id,
+            fetch_fields=fetch_fields,
+        )
         self.template = template
         return template
 
-    async def fetch_message(self, bot: discord.Client) -> Optional[discord.Message]:
+    async def fetch_message(
+            self,
+            bot: discord.Client) -> discord.Message | None:
         """
         Fetch the message associated with this profile's archivation/submission.
 
@@ -142,13 +181,18 @@ class UserProfile(object):
 
         # Get channel
         try:
-            channel = bot.get_channel(self.posted_channel_id) or await bot.fetch_channel(self.posted_channel_id)
+            channel = (
+                bot.get_channel(self.posted_channel_id)
+                or
+                await bot.fetch_channel(self.posted_channel_id)
+            )
         except discord.HTTPException:
             return None
+        assert isinstance(channel, discord.abc.MessageableChannel)
 
         # Get message
         try:
-            return await channel.fetch_message(self.posted_message_id)  # type: ignore
+            return await channel.fetch_message(self.posted_message_id)
         except discord.HTTPException:
             pass
 
@@ -173,15 +217,20 @@ class UserProfile(object):
         return {
             i: o
             for i, o in self.all_filled_fields.items()
-            if o.field is not None and o.field.deleted is False and o.value is not None
+            if
+                o.field is not None
+            and
+                o.field.deleted is False
+            and
+                o.value is not None
         }
 
+    @vbu.i18n(__name__, 2)
     def build_embed(
             self,
             bot: vbu.Bot,
-            ctx: Union[discord.Interaction, commands.Context, str],
-            member: Optional[discord.Member] = None,
-            use_guild: bool = False) -> vbu.Embed:
+            ctx: discord.Interaction | commands.Context | str,
+            member: discord.Member | None = None) -> vbu.Embed:
         """
         Converts the filled profile into an embed.
         """
@@ -193,7 +242,11 @@ class UserProfile(object):
             raise ValueError("Invalid member object passed to build embed")
 
         # Create the initial embed
-        fields: List[FilledField] = sorted(self.filled_fields.values(), key=operator.attrgetter("field.index"))
+        fields: List[FilledField]
+        fields = sorted(
+            self.filled_fields.values(),
+            key=operator.attrgetter("field.index"),
+        )
         embed = vbu.Embed(use_random_colour=True)
         if not self.template:
             raise AttributeError("Missing template field for user profile")
@@ -203,7 +256,7 @@ class UserProfile(object):
 
         # Add the user
         embed.add_field(
-            name=vbu.translation(ctx, "user_profile", use_guild=use_guild).gettext("Discord User"),
+            name=_("Discord User"),
             value=f"<@{self.user_id}>",
         )
 
@@ -225,13 +278,70 @@ class UserProfile(object):
                 continue
 
             # Set data
-            if isinstance(f.field.field_type, ImageField) or f.field.field_type == ImageField:
+            if f.field.field_type == ImageField:
                 embed.set_image(url=field_value)
             else:
-                embed.add_field(name=f.field.name, value=field_value, inline=len(field_value) <= 100)
+                embed.add_field(
+                    name=f.field.name,
+                    value=field_value,
+                    inline=len(field_value) <= 100,
+                )
 
         # Add a footer to our embed
         bot.set_footer_from_config(embed)
 
         # Return embed
         return embed
+
+    async def update(self, db: vbu.Database, **kwargs) -> Self:
+        """
+        Update multiple attributes of the profile instance both in cache
+        and in the database.
+        """
+
+        for i, o in kwargs.items():
+            setattr(self, i, o)
+        await db.call(
+            """
+            INSERT INTO
+                templates
+                (
+                    user_id,
+                    name,
+                    template_id,
+                    verified,
+                    posted_message_id,
+                    posted_channel_id,
+                    deleted
+                )
+            VALUES
+                (
+                    $1,
+                    $2,
+                    $3,
+                    $4,
+                    $5,
+                    $6,
+                    $7
+                )
+            ON CONFLICT
+                (user_id, name, template_id)
+            DO UPDATE
+            SET
+                user_id = $1,
+                name = $2,
+                template_id = $3,
+                verified = $4,
+                posted_message_id = $5,
+                posted_channel_id = $6,
+                deleted = $7
+            """,
+            self.user_id,
+            self.name,
+            self.template_id,
+            self.verified,
+            self.posted_message_id,
+            self.posted_channel_id,
+            self.deleted,
+        )
+        return self
